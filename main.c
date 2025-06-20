4b629659da667f4181f128b8f6b25569a2ff38ab
yasinkits17@gmail.com <yasinkits17@gmail.com>
2025-06-21 00:02:10 +0300

    Loan Eligibility
    Computing if the user is eligible for a loan

--- main.c
+++ main.c
@@ -2,21 +2,12 @@
 #include <stdlib.h>
 int main()
 {
-    int Employee_Years_of_service;
-    int Basic_salary;
-    int Final_Salary;
-    float Bonus ;
-    printf("Enter the the Years Of Service:\n");
-    scanf("%d", &Employee_Years_of_service);
-    printf("Enter the Basic Salary:\n");
-    scanf("%d" ,&Basic_salary);
-
-    if (Employee_Years_of_service > 5){
-     Bonus = 0.05 * Basic_salary;
-     Final_Salary = Basic_salary + Bonus;
-      printf("The Final salary for the employee is:%.2d\n", Final_Salary);
-    }else{
       printf("5 years of service not yet met please continue working with us\n");
+    if(Salary >= 5000 || Years_of_employment>2){
+    printf("You are eligible for the loan.\n");
+    }else {
+     printf("Sorry, you are not eligible for the loan.\n");
     }
-    return 0;
+
+return 0;
 }

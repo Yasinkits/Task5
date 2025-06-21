c559d435bab86b3cb836a95cafb560a2e931f080
yasinkits17@gmail.com <yasinkits17@gmail.com>
2025-06-21 16:40:20 +0300

    Profit and Loss
    Using selling Price and Cost Price to compute Profit or Los

--- main.c
+++ main.c
@@ -2,18 +2,17 @@
 #include <stdlib.h>
 int main()
 {
- int hourly_rate, hours_worked,Salary;
- printf("Please enter the hours worked:\n");
- scanf("%d", &hours_worked);
- printf("Please enter the hourly rate:\n");
- scanf("%d", &hourly_rate);
+  double Cost_price,Selling_price;
+  printf("Enter the Cost price:\n");
+  scanf("%lf", &Cost_price);
+  printf("Enter the Selling Price:\n");
+  scanf("%lf", &Selling_price);
+  double Amount= Selling_price-Cost_price;
+  if(Amount > 0){
+    printf("Profit : %.2lf\n", Amount);
+  }else if(Amount < 0){
+    printf("Loss: %.2lf",-Amount);
+  }
 
- if (hours_worked>40){
-    int overtime = hours_worked -40;
-    Salary = (40*hourly_rate + overtime*hourly_rate*1.5);
-    }else{
-    Salary= hours_worked*hourly_rate;
-    }
-    printf("The Total salary is : %.2d\n", Salary);
 return 0;
 }
